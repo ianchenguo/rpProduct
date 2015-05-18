62701418b53d83dde42818a5d268b546993e3d4a
GccOC <kylingourmand@gmail.com>
2015-05-18 14:45:20 +1000

    add command, with jshint issues, 6wrongs and tons of warning. The project is still able to be open, but very slow.

--- app/app.module.js
+++ app/app.module.js
@@ -14,6 +14,7 @@
       'app.questionA',
       'app.questionB',
       'app.questionC',
+      'app.questionCDatabaseServiceJson',
       'app.report'
     ]);
 
--- app/scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/ceQuestionCCommand.directive.js
+++ app/scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/ceQuestionCCommand.directive.js
@@ -4,15 +4,15 @@
 
 (function () {
   'use strict';
-  angular
-    .module('app.questionC')
-    .directive('ceQuestionCCommand', ceQuestionCCommand);
+    // angular.module('app.questionC').directive('ceQuestionCCommand', ceQuestionCCommand);
 
-  ceQuestionCCommand.$inject = ['movePieceService', 'readableLogService'];
-  function ceQuestionCCommand(movePieceService, readableLogService) {
+    var directiveM = angular.module('app.questionC');
+
+
+
+  directiveM.directive('ceQuestionCCommand', [function(){
+    // Runs during compile
     return {
-      restrict: 'E',
-      templateUrl: 'scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/ceQuestionCCommand.html',
       scope: {
         level: '@',
         from: '=',
@@ -20,63 +20,98 @@
         idx: '@'
       },
       controllerAs: 'vm',
+      controller: function($scope, $element, qcCommandInforRequest) {
+          qcCommandInforRequest.obtainDossierService(gainData);
+
+          function gainData (dataFromService) {
+            $scope.dibs = dataFromService;
+          };
+
+          var vm = this;
+          vm.positions = ['1', '2', '3', 'X'];
+
+
+      },
+      restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
+      templateUrl: 'scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/ceQuestionCCommand.html',
+      bindToController: true,
+      // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+      link: function($scope, iElm, iAttrs, controller) {
+
+      }
+    };
+  }]);
+
+  // ceQuestionCCommand.$inject = ['movePieceService', 'readableLogService'];
+  // movePieceService, readableLogService
+  /*function ceQuestionCCommand() {
+    return {
+      restrict: 'E',
+      templateUrl: 'scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/ceQuestionCCommand.html',
+      
+      controllerAs: 'vm',
       controller: ['$scope', controller],
-      bindToController: true
+      
     };
 
-
     //////
-    function controller($scope) {
+    function controller($scope) {*/
 
 
-      var vm = this;
+      /*var vm = this;
+      vm.positions = ['1', '2', '3', 'X'];*/
+
       //all available positions
-      vm.positions = ['', '1', '2', '3', 'X'];
-      vm.updateFrom = updateFrom;
-      vm.updateTo = updateTo;
+      // vm.positions = ['', '1', '2', '3', 'X'];
+      // vm.updateFrom = updateFrom;
+      // vm.updateTo = updateTo;
 
-      var logCommandFromSelection = function logCommandFromSelection(from, to, idx) {
-        readableLogService.saveLog(
-          readableLogService.createCommandLog('commandFromSelection', {from: from, to: to, idx: idx})
-        );
-      };
 
-      var logCommandToSelection = function logCommandToSelection(from, to, idx) {
-        readableLogService.saveLog(
-          readableLogService.createCommandLog('commandToSelection', {from: from, to: to, idx: idx})
-        );
-      };
-      var logCommandsExecution = function logCommandsExecution() {
-        readableLogService.saveLog(
-          readableLogService.createCommandLog('commandsExecute', {commandCount: 1})
-        );
-      };
+      // var logCommandFromSelection = function logCommandFromSelection(from, to, idx) {
+      //   readableLogService.saveLog(
+      //     readableLogService.createCommandLog('commandFromSelection', {from: from, to: to, idx: idx})
+      //   );
+      // };
 
-      var logCommandsExecutionFinished = function logCommandsExecutionFinished() {
-        readableLogService.saveLog(
-          readableLogService.createCommandLog('commandsExecuteFinish', {commandCount: 1})
-        );
-      };
+      // var logCommandToSelection = function logCommandToSelection(from, to, idx) {
+      //   readableLogService.saveLog(
+      //     readableLogService.createCommandLog('commandToSelection', {from: from, to: to, idx: idx})
+      //   );
+      // };
+      // var logCommandsExecution = function logCommandsExecution() {
+      //   readableLogService.saveLog(
+      //     readableLogService.createCommandLog('commandsExecute', {commandCount: 1})
+      //   );
+      // };
 
-      var logCommandsExecutionError = function logCommandsExecutionError(from, to, idx) {
-        readableLogService.saveLog(
-          readableLogService.createCommandLog('commandExecuteError', {from: from, to: to, idx: idx})
-        );
-      };
+      // var logCommandsExecutionFinished = function logCommandsExecutionFinished() {
+      //   readableLogService.saveLog(
+      //     readableLogService.createCommandLog('commandsExecuteFinish', {commandCount: 1})
+      //   );
+      // };
+
+      // var logCommandsExecutionError = function logCommandsExecutionError(from, to, idx) {
+      //   readableLogService.saveLog(
+      //     readableLogService.createCommandLog('commandExecuteError', {from: from, to: to, idx: idx})
+      //   );
+      // };
 
       //////
-      function updateFrom() {
+      /*function updateFrom() {
         logCommandFromSelection(vm.from, vm.to, vm.idx);
         move();
+        console.log('updateFrom_Function');
       }
 
 
       function updateTo() {
         logCommandToSelection(vm.from, vm.to, vm.idx);
         move();
-      }
+        console.log('updateTo_Function');
+      }*/
 
-      function move() {
+      //move function start
+      /*function move() {
         if (vm.level == 0) {
 
           vm.bgColor = '';
@@ -97,9 +132,42 @@
           }
         }
 
-      }
+      }*/
+      //end move function
 
-    }
-  }
+    /*}
+  }*/
+
+
+
+
+    directiveM.directive('repeadCreateCommandsDisplay', [function(){
+      // Runs during compile
+      return {
+        scope: {dibhere: '='}, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        restrict: 'A',
+        // template: ,
+        templateUrl: './scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/ng-commands-td.html',
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, iElm, iAttrs, controller) {
+            var commandTdSlab = iElm.find('td');
+        }
+      };
+    }]);
+
+    directiveM.directive('ionicHeaderListCommandSlab', [function(){
+      // Runs during compile
+      return {
+        scope: {},
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+        templateUrl: 'scripts/webComponents/questionC/directives/ceQuestionCCommandDirective/command-list-command-header.html',
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, iElm, iAttrs, controller) {
+          
+        }
+      };
+    }]);
 
 }());
