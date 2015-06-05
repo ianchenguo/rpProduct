93bf19df96b7d7b7209dab96289cba05dbbb788f
guochen <ianchenguo@gmail.com>
2015-06-05 13:11:33 +1000

    iteration 0.11 feature update: welcome page content update

--- app/scripts/webComponents/welcome/WelcomeController.js
+++ app/scripts/webComponents/welcome/WelcomeController.js
@@ -8,58 +8,7 @@
     .module('app.welcome')
     .controller('WelcomeController', WelcomeController);
 
-  WelcomeController.$inject = [
-    '$state',
-    '$mdDialog',
-    '$mdToast',
-    'audioRecordingService'];
-  function WelcomeController($state,
-                             $mdDialog,
-                             $mdToast,
-                             audioRecordingService) {
+  function WelcomeController() {
     var vm = this;
-    var _showConfirm = function (ev) {
-      // Appending dialog to document.body to cover sidenav in docs app
-      var confirm = $mdDialog.confirm()
-        .parent(angular.element(document.body))
-        .title('Would you like to let the app record audio?')
-        .content('This app can record audio in background during the quiz session')
-        .ariaLabel('Audio Recording Confirmation')
-        .ok('Permit')
-        .cancel('Deny')
-        .targetEvent(ev);
-      $mdDialog.show(confirm).then(function () {
-
-        audioRecordingService.startRecord('documents://tempQuizAudio.m4a');
-        _showRecordEndingToast();
-        $state.go('app.quizInfo');
-
-      }, function () {
-        $state.go('app.quizInfo');
-      });
-    };
-
-    function _showRecordEndingToast() {
-      $mdToast.show(
-        $mdToast.simple()
-          .content('Audio recording started!')
-          .position('false true false true')
-          .hideDelay(3000)
-      );
-    }
-
-    vm.startQuiz = startQuiz;
-
-
-    //////
-
-
-    function startQuiz(ev) {
-      //_showConfirm(ev);
-      $state.go('app.quizInfo');
-
-    }
-
-
   }
 }());
